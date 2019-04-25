9d135fd576e16ee2589f0fc90b6ea6f0854dbb33
ErtugrulKiziltunc <ertkiziltunc@gmail.com>
2019-04-25 12:35:37 +0200

    Repository

--- Northwind.Repository/RepositoryBase.cs
+++ Northwind.Repository/RepositoryBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Northwind.Entity;
 
 namespace Northwind.Repository
 {
@@ -20,25 +22,80 @@ namespace Northwind.Repository
          * 
          */
 
+        private NorthwindEntities context;
+        public  NorthwindEntities Context
+        {
+            get
+            {
+                //if (context == null)
+                //{
+                //    context = new NorthwindEntities();
+                //}
 
+                //return context;
+                return context ?? new NorthwindEntities();
+            }
+            set
+            {
+                context = value;
+            }
+        }
         public bool Adding(TT entity)
         {
-            throw new NotImplementedException();
+            Context.Set<TT>().Add(entity);
+            // Set<TT>(): >Context'in TT tipini algılamasını sağlar
+            try
+            {
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception )
+            {
+                return false;
+                
+            }
+          
         }
 
         public bool Deleting(TT entity)
         {
-            throw new NotImplementedException();
+            Context.Set<TT>().Remove(entity);
+            try
+            {
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
         }
 
         public List<TT> Listing()
         {
-            throw new NotImplementedException();
+            // Product entity gelirse productlar listelenecek , categoprr entity gelirse categoryler listelenecek.
+            return Context.Set<TT>().ToList();
+           
         }
 
         public bool Updating(TT entity)
         {
-            throw new NotImplementedException();
+
+            //Context.Set<TT>().Attach(entity);
+            //Context.Entry(entity).State = EntityState.Modified;
+           
+
+            try
+            {
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
         }
     }
 }
